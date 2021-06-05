b8138a583b1784476046c3f1e9e2e22565ad42c8
MySQL <1363351571@qq.com>
2021-06-05 12:10:32 +0800

    这是关于类与方法的提交

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-def my_fact(value_1):
-    if value_1 == 1:
-        return 1
-    else:
-        return value_1 * my_fact(value_1 - 1)
-
-
-pass
-pass
-x = 1
-while x == 1:
-    value_1 = int(input("你可以输入你需要计算阶乘的value!\n"))
-    answer = my_fact(value_1)
-    print(value_1, "的阶乘是", answer)
-    option = int(input('如果你要继续调试程序键入‘1’，任意键退出程序！\n'))
-    if option != 1:
-        x = 0
+#!usr/bin/env python
+#-*- coding:utf-8 -*-
+class student(object):
+    def __init__ (self,name,id,score):
+        self.__name=name
+        self.__id=id
+        self.__score=score
+    def print_message(self):
+        print("%s的身份id为%s\n成绩为%s"%(self.__name,self.__id,self.__score))
+    def set_score(self,score):
+        self.__score=score
+    def print_grade(self):
+        if self.__score>=60 and self.__score<80:
+            return 'C'
+        elif self.__score>=90:
+            return 'A'
+        elif self.__score>=80 and self.__score<90:
+            return 'B'
+        else:
+            return 'D'
+张三=student('张三',1,88)
+李四=student('李四',2,98)
+张三.print_message()
+print('获得评价',张三.print_grade())
+李四.print_message()
+print('获得评价',李四.print_grade())
+张三.set_score(100)
+张三.print_message()
+print('获得评价',张三.print_grade())
+李四.set_score(60)
+李四.print_message()
